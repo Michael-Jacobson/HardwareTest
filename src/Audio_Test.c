eb1c182f1444f63e95552d022477f6e82b35c21e
Mike Jacobson <michael.jacobson@ultratec.com>
2025-09-11 12:48:51 -0500

    adjusted how audio is set up

--- src/Audio_Test.c
+++ src/Audio_Test.c
@@ -67,6 +67,7 @@ int AveragesRX[2] = {0, 0};
 /* Handle for the PCM device */
 snd_pcm_t *playback_handle;
 snd_pcm_t *capture_handle;
+
 short Rxbuf[BUFF_SIZE];
 short Txbuf[BUFF_SIZE];
 short DeinterlaceRxdBuf[2][PERIOD_LEN]; //0 is handset, 1 is Phone line
@@ -79,6 +80,7 @@ short RawLiveCallPOTSTxSamples[PERIOD_LEN];
 short RawLiveCallHSTxSamples[PERIOD_LEN];
 
 BOOL TxRunning = FALSE;
+BOOL RxRunning = FALSE;
 
 int POTS_Sample_Rx_Index = PHONELINE;
 int HS_Sample_Rx_Index = HANDSET;
@@ -89,6 +91,7 @@ static void *ReadAudioSamples(void *arg);
 static void *WriteAudioSamples(void *arg);
 static void *ProcessRxAudioSamples(void *arg);
 static void *ProcessTxAudioSamples(void *arg);
+int SetupAudioParams(snd_pcm_t **handle, snd_pcm_stream_t playback_capture);
 
 /********************************************************************************************************************************
  *  void function(void)
@@ -106,6 +109,10 @@ void InitSoundDrivers(void)
 {
 	sem_init(&AudioRxSem, 0, 0);
 	sem_init(&AudioTxSem, 0, 0);
+
+	SetupAudioParams(&playback_handle, SND_PCM_STREAM_PLAYBACK);
+	SetupAudioParams(&capture_handle, SND_PCM_STREAM_CAPTURE);
+	snd_pcm_link(playback_handle, capture_handle);
 
 	if(pthread_create( &RxAudioProcessingThread, NULL, ProcessRxAudioSamples, NULL ) == 0)
 	{
@@ -171,130 +178,167 @@ void CloseSoundDrivers(void)
  *
  *  Notes:  notes
  ********************************************************************************************************************************/
-void *ReadAudioSamples(void *arg)
+int SetupAudioParams(snd_pcm_t **handle, snd_pcm_stream_t playback_capture)
 {
-    //BOOL KeepGoing = TRUE;
-    int err;
-    int rate = 8000; /* Sample rate */
-    unsigned int exact_rate; /* Sample rate returned by */
-    int dir = 0;
-    //snd_pcm_uframes_t bufferedframes = BUFFER_FRAME_SIZE;
+	int err = 0;
+	int rate = 8000; /* Sample rate */
+	unsigned int exact_rate; /* Sample rate returned by */
+	int dir = 0;
+	//snd_pcm_uframes_t bufferedframes = BUFFER_FRAME_SIZE;
 	unsigned int BufferTime = AUDIO_LATENCY;
 	snd_pcm_uframes_t FrameSize = PERIOD_LEN;
-	
-    /* This structure contains information about the hardware and can be used to specify the configuration to be used for */
-    /* the PCM stream. */
-    snd_pcm_hw_params_t *hw_params;
 
-    /* Name of the PCM device, like hw:0,0 */
-    /* The first number is the number of the soundcard, the second number is the number of the device. */
-    //static char *device = "hw:0,0"; /* capture device */
-    static char *device = "default"; /* capture device */
+	/* This structure contains information about the hardware and can be used to specify the configuration to be used for */
+	/* the PCM stream. */
+	snd_pcm_hw_params_t *hw_params;
 
-    /* Open PCM. The last parameter of this function is the mode. */
-    if ((err = snd_pcm_open (&capture_handle, device, SND_PCM_STREAM_CAPTURE, 0)) < 0)
-    {
-        fprintf (stderr, "cannot open RX audio device (%s)\n", snd_strerror (err));
-        exit (1);
-    }
+	/* Name of the PCM device, like hw:0,0 */
+	/* The first number is the number of the soundcard, the second number is the number of the device. */
+	//static char *device = "hw:0,0"; /* capture device */
+	static char *device = "default"; /* capture device */
+	char type[3];
 
-    memset(Rxbuf,0,sizeof(Rxbuf));
+	memset(type, 0, sizeof(type));
 
-    /* Allocate the snd_pcm_hw_params_t structure on the stack. */
-    if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0)
-    {
-        fprintf (stderr, "cannot allocate RX hardware parameter structure (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    /* Init hwparams with full configuration space */
-    if ((err = snd_pcm_hw_params_any (capture_handle, hw_params)) < 0)
-    {
-        fprintf (stderr, "cannot initialize RX hardware parameter structure (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    /* Set access type. */
-    if ((err = snd_pcm_hw_params_set_access (capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0)
-    {
-        fprintf (stderr, "cannot set access type (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    /* Set sample format */
-    if ((err = snd_pcm_hw_params_set_format (capture_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0)
-    {
-        fprintf (stderr, "cannot set RX sample format (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    /* Set sample rate. If the exact rate is not supported by the hardware, use nearest possible rate. */
-    exact_rate = rate;
-    if ((err = snd_pcm_hw_params_set_rate_near (capture_handle, hw_params, &exact_rate, 0)) < 0)
-    {
-        fprintf (stderr, "cannot set RX sample rate (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    if (rate != exact_rate) {
-        fprintf(stderr, "The RX rate %d Hz is not supported by your hardware.\n ==> Using %d Hz instead.\n", rate, exact_rate);
-    }
-
-    /* Set period size to 80 frames. */
-    err = snd_pcm_hw_params_set_period_size_near(capture_handle, hw_params, &FrameSize, &dir);
-    if (err < 0)
-    {
-        fprintf (stderr, "cannot set RX channel period (%s)\n", snd_strerror (err));
-    }
-    if (FrameSize != PERIOD_LEN)
-    {
-        fprintf(stderr, "The RX period %d is not supported by your hardware.\n ==> Wants %ld instead. Exiting\n", PERIOD_LEN, FrameSize);
-        exit (1);
-    }
-
-    /* Set number of channels */
-    if ((err = snd_pcm_hw_params_set_channels(capture_handle, hw_params, NUM_OF_CHANNELS)) < 0)
-    {
-        fprintf (stderr, "cannot set RX channel count (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-
-	if( (err = snd_pcm_hw_params_set_buffer_time_near (capture_handle, hw_params, &BufferTime, &dir)) < 0)
+	if(playback_capture == SND_PCM_STREAM_CAPTURE)
 	{
-		fprintf (stderr, "cannot set RX buffer time near (%s)\n", snd_strerror (err));
+		strcpy(type, "RX");
+	}
+	else
+	{
+		strcpy(type, "TX");
+	}
+
+	/* Open PCM. The last parameter of this function is the mode. */
+	if ((err = snd_pcm_open (handle, device, playback_capture, 0)) < 0)
+	{
+		fprintf (stderr, "cannot open %s audio device (%s)\n", type, snd_strerror (err));
+		exit (1);
+	}
+
+	memset(Rxbuf,0,sizeof(Rxbuf));
+
+	/* Allocate the snd_pcm_hw_params_t structure on the stack. */
+	if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0)
+	{
+		fprintf (stderr, "cannot allocate %s hardware parameter structure (%s)\n", type, snd_strerror (err));
+		exit (1);
+	}
+
+	/* Init hwparams with full configuration space */
+	if ((err = snd_pcm_hw_params_any (*handle, hw_params)) < 0)
+	{
+		fprintf (stderr, "cannot initialize %s hardware parameter structure (%s)\n", type, snd_strerror (err));
+		exit (1);
+	}
+
+	/* Set access type. */
+	if ((err = snd_pcm_hw_params_set_access (*handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0)
+	{
+		fprintf (stderr, "cannot set %s access type (%s)\n", type, snd_strerror (err));
+		exit (1);
+	}
+
+	/* Set sample format */
+	if ((err = snd_pcm_hw_params_set_format (*handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0)
+	{
+		fprintf (stderr, "cannot set %s sample format (%s)\n", type, snd_strerror (err));
+		exit (1);
+	}
+
+	/* Set sample rate. If the exact rate is not supported by the hardware, use nearest possible rate. */
+	exact_rate = rate;
+	if ((err = snd_pcm_hw_params_set_rate_near (*handle, hw_params, &exact_rate, 0)) < 0)
+	{
+		fprintf (stderr, "cannot set %s sample rate (%s)\n", type, snd_strerror (err));
+		exit (1);
+	}
+
+	if (rate != exact_rate) {
+		fprintf(stderr, "The %s rate %d Hz is not supported by your hardware.\n ==> Using %d Hz instead.\n", type, rate, exact_rate);
+	}
+
+	/* Set period size to 80 frames. */
+	err = snd_pcm_hw_params_set_period_size_near(*handle, hw_params, &FrameSize, &dir);
+	if (err < 0)
+	{
+		fprintf (stderr, "cannot set %s channel period (%s)\n", type, snd_strerror (err));
+	}
+	if (FrameSize != PERIOD_LEN)
+	{
+		fprintf(stderr, "The %s period %d is not supported by your hardware.\n ==> Wants %ld instead. Exiting\n", type, PERIOD_LEN, FrameSize);
+		exit (1);
+	}
+
+	/* Set number of channels */
+	if ((err = snd_pcm_hw_params_set_channels(*handle, hw_params, NUM_OF_CHANNELS)) < 0)
+	{
+		fprintf (stderr, "cannot set %s channel count (%s)\n", type, snd_strerror (err));
+		exit (1);
+	}
+
+
+	/*if( (err = snd_pcm_hw_params_set_buffer_time_near (*handle, hw_params, &BufferTime, &dir)) < 0)
+	{
+		fprintf (stderr, "cannot set %s buffer time near (%s)\n", type, snd_strerror (err));
 	}
 	if(BufferTime != AUDIO_LATENCY)
 	{
-		printf("The Rx Buffer time is not Supported. using %d instead\n", BufferTime);
+		printf("The %s Buffer time is not Supported. using %d instead\n", type, BufferTime);
+	}*/
+
+	/* Apply HW parameter settings to PCM device and prepare device. */
+	if ((err = snd_pcm_hw_params (*handle, hw_params)) < 0)
+	{
+		fprintf (stderr, "cannot set %s parameters (%s)\n", type, snd_strerror (err));
+		exit (1);
 	}
 
-    /* Apply HW parameter settings to PCM device and prepare device. */
-    if ((err = snd_pcm_hw_params (capture_handle, hw_params)) < 0)
-    {
-        fprintf (stderr, "cannot set RX parameters (%s)\n", snd_strerror (err));
-        exit (1);
-    }
+	printf("About to free params\n");
+	snd_pcm_hw_params_free(hw_params);
+	printf("params free\n");
 
-    if ((err = snd_pcm_prepare (capture_handle)) < 0)
-    {
-        fprintf (stderr, "cannot prepare RX audio interface for use (%s)\n", snd_strerror (err));
-        exit (1);
-    }
+	return 1;
+}
+
+/********************************************************************************************************************************
+ *  void function(void)
+ *
+ *  Desc
+ *
+ *  Parameters:
+ *      - none
+ *
+ *  Returns:  none
+ *
+ *  Notes:  notes
+ ********************************************************************************************************************************/
+void *ReadAudioSamples(void *arg)
+{
+    //BOOL KeepGoing = TRUE;
+	int err = 0;
 
     /*
-     * we need playback to start before
-     * the capture because the ssi renesas patch says it needs to. we also need to start them at the same time
-     * or else the interleaved samples where we assume odd samples are path1, even samples are path2 for both
-     * rx and tx won't be true. it will be random and sometimes it will work out and sometimes it will be
-     * opposite
-     */
-    while(!TxRunning)
-    {
-    	usleep(1);
-    }
+	 * we need playback to start before
+	 * the capture because the ssi renesas patch says it needs to. we also need to start them at the same time
+	 * or else the interleaved samples where we assume odd samples are path1, even samples are path2 for both
+	 * rx and tx won't be true. it will be random and sometimes it will work out and sometimes it will be
+	 * opposite
+	 */
+	/*while(!TxRunning)
+	{
+		usleep(1);
+	}
+
+	//usleep(10000);
+*/
+
+
+
 
     //snd_pcm_start(capture_handle);
+
+	printf("starting capture\n");
 
     /***************************************************************************************************************/
     /***************************************************************************************************************/
@@ -335,6 +379,7 @@ void *ReadAudioSamples(void *arg)
 			}
 			else
 			{
+				RxRunning = TRUE;
 				RxGoodCount++;
 				//printf("Captures %d\n", RxGoodCount);
 				sem_post(&AudioRxSem);
@@ -351,8 +396,9 @@ void *ReadAudioSamples(void *arg)
         }
     }
 
+    RxRunning = FALSE;
+
     snd_pcm_drop(capture_handle);
-	snd_pcm_hw_params_free (hw_params);
 	snd_pcm_close (capture_handle);
 
 
@@ -374,114 +420,18 @@ void *ReadAudioSamples(void *arg)
 void *WriteAudioSamples(void *arg)
 {
     //BOOL KeepGoing = TRUE;
-    int err;
-    int rate = 8000; /* Sample rate */
-    unsigned int exact_rate; /* Sample rate returned by */
-    int dir = 0;
-    /* Playback stream */
-    snd_pcm_stream_t stream = SND_PCM_STREAM_PLAYBACK;
-    /* This structure contains information about the hardware and can be used to specify the configuration to be used for */
-    /* the PCM stream. */
-    snd_pcm_hw_params_t *hw_params;
-    //snd_pcm_uframes_t bufferedframes = BUFFER_FRAME_SIZE;
-	unsigned int BufferTime = AUDIO_LATENCY;
-	snd_pcm_uframes_t FrameSize = PERIOD_LEN;
-    /* Name of the PCM device, like plughw:0,0 */
-    /* The first number is the number of the soundcard, the second number is the number of the device. */
-    static char *device = "default"; /* playback device */
+	int err = 0;
 
-    /* Open PCM. The last parameter of this function is the mode. */
-    if ((err = snd_pcm_open (&playback_handle, device, stream, 0)) < 0)
-    {
-        fprintf (stderr, "cannot open TX audio device (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    /* Allocate the snd_pcm_hw_params_t structure on the stack. */
-    if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0)
-    {
-        fprintf (stderr, "cannot allocate TX hardware parameters (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    /* Init hwparams with full configuration space */
-    if ((err = snd_pcm_hw_params_any (playback_handle, hw_params)) < 0)
-    {
-        fprintf (stderr, "cannot initialize TX hardware parameter structure (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    /* Set access type. */
-    if ((err = snd_pcm_hw_params_set_access (playback_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0)
-    {
-        fprintf (stderr, "cannot set TX access type (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    /* Set sample format */
-    if ((err = snd_pcm_hw_params_set_format (playback_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0)
-    {
-        fprintf (stderr, "cannot set TX sample format (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    /* Set sample rate. If the exact rate is not supported by the hardware, use nearest possible rate. */
-    exact_rate = rate;
-    if ((err = snd_pcm_hw_params_set_rate_near (playback_handle, hw_params, &exact_rate, 0)) < 0)
-    {
-        fprintf (stderr, "cannot set TX sample rate (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    if (rate != exact_rate)
-    {
-        fprintf(stderr, "The Tx rate %d Hz is not supported by your hardware.\n ==> Using %d Hz instead.\n", rate, exact_rate);
-    }
-
-    /* Set period size to 80 frames. */
-    err = snd_pcm_hw_params_set_period_size_near(playback_handle, hw_params, &FrameSize, &dir);
-    if (err < 0)
-    {
-        fprintf (stderr, "cannot set TX channel period (%s)\n", snd_strerror (err));
-    }
-    if (FrameSize != (PERIOD_LEN))
-    {
-        fprintf(stderr, "The TX period %d is not supported by your hardware.\n ==> Wants %ld instead. Exiting\n", (PERIOD_LEN), FrameSize);
-        exit (1);
-    }
-
-    /* Set number of channels */
-    if ((err = snd_pcm_hw_params_set_channels (playback_handle, hw_params, NUM_OF_CHANNELS)) < 0)
-    {
-        fprintf (stderr, "cannot set TX channel count (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    if( (err = snd_pcm_hw_params_set_buffer_time_near (playback_handle, hw_params, &BufferTime, &dir)) < 0)
+    /*while(!RxRunning)
 	{
-		fprintf (stderr, "cannot set TX buffer time near (%s)\n", snd_strerror (err));
-	}
-    if(BufferTime != AUDIO_LATENCY)
-	{
-		printf("The Tx Buffer time is not Supported. using %d instead\n", BufferTime);
+		usleep(1);
 	}
 
-    //Now we apply the configuration to the PCM device pointed to by pcm_handle and prepare the PCM device.
+	usleep(10000);*/
 
-    /* Apply HW parameter settings to PCM device and prepare device. */
-    if ((err = snd_pcm_hw_params (playback_handle, hw_params)) < 0)
-    {
-        fprintf (stderr, "cannot set TX parameters (%s)\n", snd_strerror (err));
-        exit (1);
-    }
+    //snd_pcm_start(playback_handle);
 
-    if ((err = snd_pcm_prepare (playback_handle)) < 0)
-    {
-        fprintf (stderr, "cannot prepare TX audio interface for use (%s)\n", snd_strerror (err));
-        exit (1);
-    }
-
-    snd_pcm_start(playback_handle);
+	printf("starting playback\n");
 
     /***************************************************************************************************************/
     /***************************************************************************************************************/
@@ -524,7 +474,7 @@ void *WriteAudioSamples(void *arg)
 			{
 				if(TxAudioProcessingThread != 0)
 				{
-					snd_pcm_start(capture_handle);
+					//snd_pcm_start(capture_handle);
 					TxRunning = TRUE;
 					TxGoodCount++;
 					sem_post(&AudioTxSem);
@@ -539,13 +489,14 @@ void *WriteAudioSamples(void *arg)
 
     TxRunning = FALSE;
     //snd_pcm_drain(playback_handle);
-	snd_pcm_hw_params_free (hw_params);
 	snd_pcm_close (playback_handle);
 
 
     return 0;
 }
 
+#include "Codec.h"
+CodecControlStates_t CodecControlState;
 /********************************************************************************************************************************
  *  void function(void)
  *
@@ -589,7 +540,7 @@ void *ProcessRxAudioSamples(void *arg)
 		AveragesRX[HANDSET] = avg2/(PERIOD_LEN);
 
 #define MAX_WAIT_FOR_CORRECTION	100
-#if 0
+
 		if(RunOnce <= MAX_WAIT_FOR_CORRECTION)
 		{
 			RunOnce++;
@@ -599,20 +550,24 @@ void *ProcessRxAudioSamples(void *arg)
 			 * the samples start with POTS, sometimes it starts with Handset. there is no way
 			 * to know which is which. so we have to play this averages game to figure it out
 			 */
-			if((AveragesRX[PHONELINE] == 0) && (AveragesRX[HANDSET] != 0))
+			if((AveragesRX[PHONELINE] == 0) && (AveragesRX[HANDSET] != 0) && (CodecControlState == CODEC_IDLE))
 			{
+#if 0
 				printf("Switching Inputs on #%d!\n", RunOnce);
 				POTS_Sample_Rx_Index = HANDSET;
 				HS_Sample_Rx_Index = PHONELINE;
+#else
+				printf("\n\n\nFound Reversed Rx: %d\n\n\n", RunOnce);
+#endif
 				RunOnce = MAX_WAIT_FOR_CORRECTION+1;
 			}
 		}
 		if(RunOnce == MAX_WAIT_FOR_CORRECTION)
 		{
-			printf("Done Looking For Channel Swap\n");
+			printf("---------->Done Looking For Channel Swap\n");
 			RunOnce++;
 		}
-#endif
+
 		memcpy(RawLiveCallHSSamples, DeinterlaceRxdBuf[HS_Sample_Rx_Index], (PERIOD_LEN*2));
 		memcpy(RawLiveCallPOTSSamples, DeinterlaceRxdBuf[POTS_Sample_Rx_Index], (PERIOD_LEN*2));
 
